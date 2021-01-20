a64088f6f279af72a6825cccc515e47dfee5e9e5
Poncho <jplyles1212@gmail.com>
2021-01-19 18:46:55 -0800

    Write program to get input and calculate gross pay

--- exercises.py
+++ exercises.py
@@ -1,5 +1,11 @@
 # 2.15 Exercises
 
 # Write a program that uses input to prompt a user for their name and then welocmes them
-name = input('Enter your name:' )
+name = input('Enter your name: ' )
 print('Hello', name)
+
+# Write a program to prompt the user for hours and rate per hour to compute gross pay
+hours = input('Enter Hours: ', hours)
+rate = input('Enter Rate: ', rate)
+pay = hours * rate
+print('Pay: ', pay)
