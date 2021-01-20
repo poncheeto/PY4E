6aecd5c7624d7551551d32465ef333731c14d498
Poncho <jplyles1212@gmail.com>
2021-01-19 18:53:27 -0800

    Change int cast to float

--- exercises.py
+++ exercises.py
@@ -7,5 +7,5 @@
 # Write a program to prompt the user for hours and rate per hour to compute gross pay
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
-pay = int(hours) * int(rate)
+pay = int(hours) * float(rate)
 print('Pay: ', pay)
