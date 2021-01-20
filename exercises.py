4c32e43a9bb4b72c0812e40ed13acf71bf1c9b0d
Poncho <jplyles1212@gmail.com>
2021-01-19 18:52:18 -0800

    Remove variables and convert str to int values on line 10

--- exercises.py
+++ exercises.py
@@ -1,11 +1,11 @@
 # 2.15 Exercises
 
 # Write a program that uses input to prompt a user for their name and then welocmes them
-name = input('Enter your name: ' )
-print('Hello', name)
+# name = input('Enter your name: ' )
+# print('Hello', name)
 
 # Write a program to prompt the user for hours and rate per hour to compute gross pay
-hours = input('Enter Hours: ', hours)
-rate = input('Enter Rate: ', rate)
-pay = hours * rate
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = int(hours) * int(rate)
 print('Pay: ', pay)
