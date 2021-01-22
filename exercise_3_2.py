acc0fcaece0087c66c3a194192d4e0941b8bfe55
Poncho <jplyles1212@gmail.com>
2021-01-21 18:56:18 -0800

    Change cast to lines 4 and 5

--- exercise_3_2.py
+++ exercise_3_2.py
@@ -1,13 +1,18 @@
-# Rewrite your pay program usingtryandexceptso that yourprogram handles non-numeric input gracefully by printing a messageand exiting the program. The following shows two executions of theprogram:
+# Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
 
-hours = input('Enter Hours: ')
-rate = input('Enter Rate: ')
-regular_pay = int(hours) * float(rate)
+try :
+    hours = int(input('Enter Hours: '))
+    rate = float(input('Enter Rate: '))
+    regular_pay = (hours) * (rate)
 
-if int(hours) > 40 :
-    overtime_hours = int(hours) - 40
-    print(overtime_hours)
-    overtime_pay = (1.5 * int(overtime_hours)) + regular_pay
-    print('Pay: ', overtime_pay)
-else  :
-    print('Pay: ', regular_pay)
+    if hours > 40 :
+        overtime_hours = hours - 40
+        print(overtime_hours)
+        
+        overtime_pay = (1.5 * overtime_hours) + regular_pay
+        print('Pay: ', overtime_pay)
+    else  :
+        print('Pay: ', regular_pay)
+
+except :
+        print('Error, please enter a numeric input')
