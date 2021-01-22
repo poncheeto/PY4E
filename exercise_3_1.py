6e8c04bf358459749241bd06c17ea0ffda3c49f1
Poncho <jplyles1212@gmail.com>
2021-01-21 17:48:43 -0800

    Create file for exercise 3.1

---
+++ exercise_3_1.py
@@ -0,0 +1,5 @@
+# Write a program to prompt the user for hours and rate per hour to compute gross pay
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+pay = int(hours) * float(rate)
+print('Pay: ', pay)
