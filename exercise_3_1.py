427b2155afd2f6a042c3997482232776413bac72
Poncho <jplyles1212@gmail.com>
2021-01-21 18:01:16 -0800

    Fix mathematical error on line 8

--- exercise_3_1.py
+++ exercise_3_1.py
@@ -5,7 +5,8 @@ rate = input('Enter Rate: ')
 regular_pay = int(hours) * float(rate)
 
 if int(hours) > 40 :
-    overtime_hours = 40 - int(hours)
+    overtime_hours = int(hours) - 40
+    print(overtime_hours)
     overtime_pay = (1.5 * int(overtime_hours)) + regular_pay
     print('Pay: ', overtime_pay)
 else  :
