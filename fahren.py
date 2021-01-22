cfcecf0d45ee595afedeae1b7eb4f2451131b862
Poncho <jplyles1212@gmail.com>
2021-01-21 16:35:18 -0800

    Add try and except statements to handle exceptions

--- fahren.py
+++ fahren.py
@@ -1,6 +1,9 @@
 inp = input('Enter Fahrenheit Temperature: ')
-fahr = float(inp)
-cel = (fahr - 32.0) * 5.0 / 9.0
-print(cel)
+try:
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
+except:
+    print('Please enter a number')
 
 # Code: http://www.py4e.com/code3/fahren.py
