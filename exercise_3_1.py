061b42142dc6f8f06f6929a2a8ee57395160e496
Poncho <jplyles1212@gmail.com>
2021-01-21 20:11:00 -0800

    Fix overtime pay math in line 12

--- exercise_3_1.py
+++ exercise_3_1.py
@@ -1,4 +1,4 @@
-# Rewrite your pay computation to give the employee 1.5times the hourly rate for hours worked above 40 hours.
+# Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
 
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
