382cb76391b5765579aaccf9e150d4a49bb941bc
Poncho <jplyles1212@gmail.com>
2021-01-21 17:58:14 -0800

    Modify to give 1.5 times pay over 40 hours

--- exercise_3_1.py
+++ exercise_3_1.py
@@ -1,5 +1,12 @@
-# Write a program to prompt the user for hours and rate per hour to compute gross pay
+# Rewrite your pay computation to give the employee 1.5times the hourly rate for hours worked above 40 hours.
+
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
-pay = int(hours) * float(rate)
-print('Pay: ', pay)
+regular_pay = int(hours) * float(rate)
+
+if int(hours) > 40 :
+    overtime_hours = 40 - int(hours)
+    overtime_pay = (1.5 * int(overtime_hours)) + regular_pay
+    print('Pay: ', overtime_pay)
+else  :
+    print('Pay: ', regular_pay)
