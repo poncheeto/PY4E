e7a6bddaacb121c03059ef9d7fee2fbc45a29c06
Poncho <jplyles1212@gmail.com>
2021-01-21 19:20:15 -0800

    Move try statement to the top of code

--- exercise_3_3.py
+++ exercise_3_3.py
@@ -1,9 +1,8 @@
 # Write a program to prompt for a score between 0.0 and1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
     # >= 0.9A >= 0.8B >= 0.7C >= 0.6D < 0.6F
 
-grade = float(input('Enter your grade: '))
-
 try :
+    grade = float(input('Enter your grade: '))
     if grade >= 0.0 and grade<= 1.0 :
         if grade >= 0.9 :
             print('A')
