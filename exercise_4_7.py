7b0471901e4612a8bc97fc57a20f4cdfd72c573c
Poncho <jplyles1212@gmail.com>
2021-01-25 18:50:46 -0800

    Print grade using built function

--- exercise_4_7.py
+++ exercise_4_7.py
@@ -1,8 +1,6 @@
 # Rewrite the grade program from the previous chapter using a function called computegrade that takes a score as its parameter and returns a grade as a string.
 
-grade = float(input('Enter your grade: '))
-
-def computegrade(score)
+def computegrade(score):
     if score >= 0.9 :
         grade = 'A'
     elif score >= 0.8 :
@@ -14,3 +12,5 @@ def computegrade(score)
     elif score < 0.6:
         grade = 'F'
     return grade
+
+print(computegrade(.70))
