fb33ed063ac6032c1630e346b9edf17567d48dd3
Poncho <jplyles1212@gmail.com>
2021-01-27 18:25:44 -0800

    Fix computational mistake in function and refactor

--- exercise_4_6.py
+++ exercise_4_6.py
@@ -1,20 +1,15 @@
 # Rewrite your pay computation with time-and-a-half for over-time and create a function called computepay which takes two parameters(hours and rate)
 def computepay(hours, rate):
-    pay = hours * (0.5 * rate)
+    pay = hours * rate
     return pay
 
-try :
-    hours = float(input('Enter Hours: '))
-    rate = float(input('Enter Rate: '))
-    regular_pay = (hours) * (rate)
+hours = float(input('Enter Hours: '))
+rate = float(input('Enter Rate: '))
+regular_pay = computepay(hours, rate)
 
-    if hours > 40 :
-        overtime_hours = hours - 40
-        
-        overtime_pay = computepay + regular_pay
-        print('Pay: ', overtime_pay)
-    else  :
-        print('Pay: ', regular_pay)
-
-except :
-        print('Error, please enter a numeric input')
+if hours > 40 :
+    overtime_hours = hours - 40
+    overtime_pay = computepay(overtime_hours, (rate * 0.5)) + regular_pay
+    print('Pay: ', overtime_pay)
+else  :
+    print('Pay: ', regular_pay)
