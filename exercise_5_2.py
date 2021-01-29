0dbd5b577b5166522e7887f41cda09da476fd124
Poncho <jplyles1212@gmail.com>
2021-01-28 19:42:34 -0800

    Print min and max values of inputted numbers

--- exercise_5_2.py
+++ exercise_5_2.py
@@ -1,4 +1,19 @@
-min = 0
-max = 0
+min = None
+max = None
 
-str_value = 
+while True:
+    str_value = input('Enter a number: ')
+    if str_value == 'done':
+        break
+    try:
+        int_value = int(str_value)
+    except:
+        print('Invalid input')
+        continue
+
+    if min is None or int_value < min:
+        min = int_value
+    elif max is None or int_value > max:
+        max = int_value
+
+print(min, max)
