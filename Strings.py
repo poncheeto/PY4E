1e7cfb47412b755f18bb241399565430adaadb33
Poncho <jplyles1212@gmail.com>
2021-02-04 05:47:38 -0800

    Create count method to print count of chosen letter in word

--- Strings.py
+++ Strings.py
@@ -1,14 +1,24 @@
 fruit = 'banana'
 
 
-# Exercise 1
-i = 0
-while i < len(fruit):
-    print(fruit[i])
-    i += 1
+# # Exercise 1
+# i = 0
+# while i < len(fruit):
+#     print(fruit[i])
+#     i += 1
 
-for char in fruit:
-    print(char)
+# for char in fruit:
+#     print(char)
 
-# Exercise 2
-print(fruit[:])
+# # Exercise 2
+# print(fruit[:])
+
+# Exericse 3
+def count(word, letter):
+    count = 0;
+    for char in word:
+        if char == letter:
+            count += 1
+    print(count)
+
+count('banana', 'a')
