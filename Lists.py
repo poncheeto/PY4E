e7d1cfe4acdf7b30f177bee04f84fa5d0cbc5981
Poncho <jplyles1212@gmail.com>
2021-02-09 06:14:56 -0800

    Create chop function to remove first and last elements but return None

--- Lists.py
+++ Lists.py
@@ -1,10 +1,18 @@
-numlist = list()
+# numlist = list()
 
-while True:
-    inp = input('Enter a number: ')
-    if inp == 'done': break
-    value = float(inp)
-    numlist.append(value)
+# while True:
+#     inp = input('Enter a number: ')
+#     if inp == 'done': break
+#     value = float(inp)
+#     numlist.append(value)
     
-average = sum(numlist) / len(numlist)
-print('Average: ', average)
+# average = sum(numlist) / len(numlist)
+# print('Average: ', average)
+
+# Exercise 6.1
+def chop(t):
+    t = t[1:(len(t) - 1)]
+
+print(chop([1, 2, 3, 4]))
+
+
