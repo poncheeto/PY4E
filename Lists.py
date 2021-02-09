0ee7b2ee43e08b5e0ed6241c1ca4828cc8eb7ea7
Poncho <jplyles1212@gmail.com>
2021-02-09 05:51:45 -0800

    Fix indent errors

--- Lists.py
+++ Lists.py
@@ -4,7 +4,7 @@ while True:
     inp = input('Enter a number: ')
     if inp == 'done': break
     value = float(inp)
-        numlist.append(value)
+    numlist.append(value)
     
-    average = sum(numlist) / len(numlist)
-    print('Average: ', average)
+average = sum(numlist) / len(numlist)
+print('Average: ', average)
