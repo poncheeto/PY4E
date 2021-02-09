fece39a5c94340b381c16125cd23bf603fc36fff
Poncho <jplyles1212@gmail.com>
2021-02-09 06:56:51 -0800

    Create program to print max and min values to console from user input

--- Lists.py
+++ Lists.py
@@ -9,14 +9,25 @@
 # average = sum(numlist) / len(numlist)
 # print('Average: ', average)
 
-# Exercise 6.1
-def chop(t):
-    t = t[1:(len(t) - 1)]
+# # Exercise 6.1
+# def chop(t):
+#     t = t[1:(len(t) - 1)]
 
-print(chop([1, 2, 3, 4]))
+# print(chop([1, 2, 3, 4]))
 
-def middle(t):
-    return t[1:(len(t) - 1)]
+# def middle(t):
+#     return t[1:(len(t) - 1)]
 
-print(middle([1, 2, 3, 4,]))
+# print(middle([1, 2, 3, 4,]))
 
+t = list()
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done': break
+    value = int(inp)
+    t.append(value)
+    max_value = max(t)
+    min_value = min(t)
+
+print('Maximum:', max_value)
+print('Minimum:', min_value)
