3f6f620fb9edd869bd75c609148a0144281bf90f
Poncho <jplyles1212@gmail.com>
2021-02-11 06:10:07 -0800

    Print lines that start with From:

--- Files.py
+++ Files.py
@@ -7,7 +7,13 @@
 #     count += 1
 # print('Line Count:', count)
 
+# fhand = open('mbox-short.txt')
+# inp = fhand.read()
+# print(len(inp))
+# print(inp[:20])
+
 fhand = open('mbox-short.txt')
-inp = fhand.read()
-print(len(inp))
-print(inp[:20])
+count = 0
+for line in fhand:
+    if line.startswith('From:'):
+        print(line)
