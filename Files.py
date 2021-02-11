a79219a50544c3e480c9e186da80d4607101dd8d
Poncho <jplyles1212@gmail.com>
2021-02-11 06:21:04 -0800

    Add error handling for user input

--- Files.py
+++ Files.py
@@ -12,9 +12,22 @@
 # print(len(inp))
 # print(inp[:20])
 
-fhand = open('mbox-short.txt')
+# fhand = open('mbox-short.txt')
+# count = 0
+# for line in fhand:
+#     line = line.rstrip()
+#     if line.startswith('From:'):
+#         print(line)
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
 count = 0
 for line in fhand:
-    line = line.rstrip()
-    if line.startswith('From:'):
-        print(line)
+    if line.startswith('Subject:'):
+        count += 1
+print('There were', count, 'subject lines in', fname)
+    
