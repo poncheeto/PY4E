ec7a91b8ca2f78e22d07edf78bd61335d713fec1
Poncho <jplyles1212@gmail.com>
2021-02-11 06:07:43 -0800

    Print number of characters in file to console

--- Files.py
+++ Files.py
@@ -1,8 +1,13 @@
 # fhand = open('mbox.txt')
 # print(fhand)
 
+# fhand = open('mbox-short.txt')
+# count = 0
+# for line in fhand:
+#     count += 1
+# print('Line Count:', count)
+
 fhand = open('mbox-short.txt')
-count = 0
-for line in fhand:
-    count += 1
-print('Line Count:', count)
+inp = fhand.read()
+print(len(inp))
+print(inp[:20])
