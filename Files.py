007203011fa4968e1a0c61871ce9beef11ce3dcf
Poncho <jplyles1212@gmail.com>
2021-02-11 06:02:13 -0800

    Add for loop to count lines in text file

--- Files.py
+++ Files.py
@@ -1,2 +1,8 @@
-fhand = open('mbox.txt')
-print(fhand)
+# fhand = open('mbox.txt')
+# print(fhand)
+
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    count += 1
+print('Line Count:', count)
