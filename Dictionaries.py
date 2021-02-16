34bec98ba376bd7cf69c65951c59adaced4e822f
Poncho <jplyles1212@gmail.com>
2021-02-16 07:04:31 -0800

    Use dictionary to count number of each letter in word

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,17 +1,26 @@
-# Exercise 1 
-fhand = open('words.txt', 'r')
+# # Exercise 1 
+# fhand = open('words.txt', 'r')
 
-lines = []
-word_dict = dict()
+# lines = []
+# word_dict = dict()
 
-for line in fhand:
-    lines.append(line)
-    for line in lines:
-        line_words = line.split()
-        for word in line_words:
-            word_dict[word] = word
+# for line in fhand:
+#     lines.append(line)
+#     for line in lines:
+#         line_words = line.split()
+#         for word in line_words:
+#             word_dict[word] = word
 
-fhand.close()
-print('skills' in word_dict)
-print('data' in word_dict)
-print('possum' in word_dict)
+# fhand.close()
+# print('skills' in word_dict)
+# print('data' in word_dict)
+# print('possum' in word_dict)
+
+word = 'brontosaurus'
+d = dict()
+for c in word:
+    if c not in d:
+        d[c] = 1
+    else:
+        d[c] += 1
+print(d)
