7f6ced00cf63ad5955d800eec20e3610d2063346
Poncho <jplyles1212@gmail.com>
2021-02-16 07:56:38 -0800

    Add code to read file and print lines starting with From

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,26 +1,42 @@
-# # Exercise 1 
-# fhand = open('words.txt', 'r')
+# # # Exercise 1 
+# fhand = open('words.txt')
 
-# lines = []
 # word_dict = dict()
 
 # for line in fhand:
-#     lines.append(line)
-#     for line in lines:
-#         line_words = line.split()
-#         for word in line_words:
-#             word_dict[word] = word
+#     words = line.split()
+#     for word in words:
+#         word_dict[word] = word
 
-# fhand.close()
 # print('skills' in word_dict)
 # print('data' in word_dict)
 # print('possum' in word_dict)
 
-word = 'brontosaurus'
-d = dict()
-for c in word:
-    if c not in d:
-        d[c] = 1
-    else:
-        d[c] += 1
-print(d)
+# word = 'brontosaurus'
+# d = dict()
+# for c in word:
+#     if c not in d:
+#         d[c] = 1
+#     else:
+#         d[c] += 1
+# print(d)
+
+# word = 'brontosaurus'
+# d = dict()
+# for c in word:
+#     d[c] = d.get(c,0) + 1
+# print(d)
+
+# Exercise 
+fname = input('Enter a file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+count = 0
+days = dict()
+for line in fhand:
+    if line.startswith('From'):
+        line = line.rstrip()
+        print(line)
