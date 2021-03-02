6c2847458ad5e8a8373c0e86b82b1a18a0027189
Poncho <jplyles1212@gmail.com>
2021-03-01 20:07:21 -0800

    Create child class and demonstrate use

--- OOP.py
+++ OOP.py
@@ -9,9 +9,16 @@ class PartyAnimal:
         self.x = self.x + 1
         print(self.name, 'party count', self.x)
 
-s = PartyAnimal('Sally')
-j = PartyAnimal('Jim')
+class FootballFan(PartyAnimal):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party
+        print(self.name,'points', self.points)
 
+s = PartyAnimal('Sally')
 s.party()
+
+j = FootballFan('Jim')
 j.party()
-s.party()
+j.touchdown()
